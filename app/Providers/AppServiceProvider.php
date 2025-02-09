246a9d400ca7b2516faf96e4ec47431ea2de5f09
ahmed <ahmedgamalfathy39@gmail.com>
2025-02-09 21:00:37 +0200

    commit navpar provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,8 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //$frontPage = FrontPage::with('translations')->where('is_active', FrontPageStatus::ACTIVE)->get();
-
-        //view()->share('navbarLinks', $frontPage);
+        $frontPage = FrontPage::with('translations')->where('is_active', FrontPageStatus::ACTIVE)->get();
+        view()->share('navbarLinks', $frontPage);
     }
 }
