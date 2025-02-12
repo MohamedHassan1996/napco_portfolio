d18169bb0c28ad22e3e5586fa82c99f85698a5ea
ahmed <ahmedgamalfathy39@gmail.com>
2025-02-12 16:53:13 +0200

    add slug to allresource

--- app/Http/Resources/Blog/AllBlogResource.php
+++ app/Http/Resources/Blog/AllBlogResource.php
@@ -16,13 +16,18 @@ class AllBlogResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-
+        $translations = $this->translations->mapWithKeys(function ($translation) {
+            return [
+                'slug' . ucfirst($translation->locale) => $translation->slug ?? "",
+            ];
+        });
 
         return [
             'blogId' => $this->id,
             'title' => $this->title,
             //'content' => $this->content??"",
-            //'slug' => $this->slug??"",
+            'slugAr' => $translations['slugAr'] ?? "",
+            'slugEn' => $translations['slugEn'] ?? "",
             'thumbnail' => $this->thumbnail?Storage::disk('public')->url($this->thumbnail):"",
             //'metaData' => $this->meta_data??[],
             'publishedAt' => $this->published_at ? Carbon::parse($this->published_at)->format('d/m/Y H:i:s') : "",
--- app/Http/Resources/Product/AllProductResource.php
+++ app/Http/Resources/Product/AllProductResource.php
@@ -18,8 +18,15 @@ class AllProductResource extends JsonResource
 
     public function toArray(Request $request): array
     {
+        $translations = $this->translations->mapWithKeys(function ($translation) {
+            return [
+                'slug' . ucfirst($translation->locale) => $translation->slug ?? "",
+            ];
+        });
         return [
             'productId' => $this->id,
+            'slugEn' => $translations['slugEn'] ?? "",
+            'slugAr' => $translations['slugAr'] ?? "",
             'name' => $this->name,
             'isActive' => $this->is_active,
             'image' => $this->images->first() != null? Storage::disk('public')->url($this->images->first()->path):"",
